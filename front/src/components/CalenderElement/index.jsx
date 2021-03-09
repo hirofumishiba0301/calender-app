3586304d74bc768c829e65b23996598007cba4c1
hirofumishiba0301 <71271771+hirofumishiba0301@users.noreply.github.com>
2021-03-10 00:12:26 +0900

    初日判定、当日・当月判定機能追加

--- front/src/components/CalenderElement/index.jsx
+++ front/src/components/CalenderElement/index.jsx
@@ -1,22 +1,32 @@
 import React from "react";
-
 import * as styles from "./style.css";
-
 import { Typography } from "@material-ui/core";
+import dayjs from "dayjs";
+import { isSameMonth, isFirstDay, isSameDay } from "../../services/calender"
 
 
 const CalenderElement = ({ day }) => {
-  const isFirstDay = day.date() === 1;
+  const today = dayjs();
+  //初日表示変更
   const format = isFirstDay ? "M月D日" : "D";
+  //今日判定
+  const isToday = isSameDay(day, today);
+  //今月以外グレー判定
+  const isCurrentMonth = isSameMonth(day, today);
+  const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+
   return (
     <div className={styles.element}>
       <Typography 
       className={styles.date}
+      color={textColor}
       align="center"
       variant="caption"
       component="div"
       >
-        {day.format("D")}
+        <span className={isToday ? styles.today : ""}>
+          {day.format("D")}
+        </span>
       </Typography>
     </div>
   );
